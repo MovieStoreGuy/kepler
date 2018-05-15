6f4d6e342aaf6bb8c267164701f6d7fd00448fa9
Sean Marciniak <sean@beamery.com>
2018-05-15 16:26:01 +0100

    Adding my ideas on how to do this

--- commands/node/tester.go
+++ commands/node/tester.go
@@ -2,13 +2,19 @@
 // git repo with submodules to automate potentially repeative or mundane tasks.
 package node
 
-import blast "github.com/LGUG2Z/blastradius"
+import (
+	"runtime"
+	"sync"
+
+	blast "github.com/LGUG2Z/blastradius"
+)
 
 // TestedProject contains all the data required to
 // send back to main process and report weather or not it failed
 type TestedProject struct {
 	Name     string
 	ExitCode int
+	Output   []byte
 }
 
 // RunTestsOn will test the given project
@@ -18,7 +24,24 @@ func RunTestsOn(project string, command ...string) (chan TestedProject, error) {
 	if err != nil {
 		return nil, err
 	}
-	results := make(chan TestedProject)
-	_ = projects
+	results := make(chan TestedProject, runtime.NumCPU())
+	wg := sync.WaitGroup{}
+	// detaching the dispatcher thread from the main as
+	// as to avoid blocking the main thread
+	go func(ch chan TestedProject, projects []string) {
+		wg.Add(len(projects))
+		for _, p := range project {
+			go func(wg *sync.WaitGroup, ch chan TestedProject) {
+
+			}(&wg, ch)
+			wg.Done()
+		}
+		wg.Wait()
+		close(ch)
+	}(results, append(projects, project))
 	return results, nil
+}
+
+func executeTests(project string, cmd ...string) (TestedProject, error) {
+	return TestedProject{}, nil
 }
